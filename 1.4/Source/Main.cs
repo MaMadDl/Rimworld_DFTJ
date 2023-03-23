e75848b87a5274885813be005d3e300ff21fb9e5
Mohammad Ali Gholami <mamaddl@live.com>
2023-03-23 20:17:07 +0430

    Settings Added

--- 1.4/Source/Main.cs
+++ 1.4/Source/Main.cs
@@ -16,24 +16,29 @@ namespace RoomApparels
 		public static RoomApparelsSettings settings;
 		public RoomApparelsMain(ModContentPack content) : base(content)
 		{
-			new Harmony("PrisonSelector.Mod").PatchAll();
-		}
-		public override string SettingsCategory() => "Room Apparels Job Type Selector";
+            settings = GetSettings<RoomApparelsSettings>();
+            
+            new Harmony("PrisonSelector.Mod").PatchAll();
+        }
+		public override string SettingsCategory() => "Room Apparels Jobs";
 
         public override void DoSettingsWindowContents(Rect inRect)
         {
             base.DoSettingsWindowContents(inRect);
 
-            //if (settings.jobTypeList == null) settings.jobTypeList = new Dictionary<string, bool>();
-            //foreach (string defName in toggleablespawndef.toggleablePawns)
-            //{
-            //    if (!settings.jobTypeList.ContainsKey(defName) && DefDatabase<JobDef>.GetNamedSilentFail(defName) != null)
-            //    {
-            //        settings.jobTypeList[defName] = false;
-            //    }
-            //}
+            List<SkillDef> jobList = DefDatabase<SkillDef>.AllDefsListForReading;
+            
 
 
+            if (settings.jobTypeList == null) settings.jobTypeList = new Dictionary<string, bool>();
+            foreach (var def in jobList)
+            {
+                string name = def.ToString();
+                if (!settings.jobTypeList.ContainsKey(name) && DefDatabase<SkillDef>.GetNamedSilentFail(name) != null)
+                {
+                    settings.jobTypeList[name] = false;
+                }
+            }
 
             settings.DoWindowContents(inRect);
 
---
+++ 1.4/Source/RoomApparelsSettings.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace RoomApparels
+{
+    public class RoomApparelsSettings : ModSettings
+    {
+        private static Vector2 scrollPos = Vector2.zero;
+        public Dictionary<string,bool> jobTypeList = new Dictionary<string, bool>();
+        public bool allPossibleJobs = false;
+
+        private List<string> jobTypes;
+        private List<bool> jobBoolValue;
+        public override void ExposeData()
+        {            
+            base.ExposeData();
+            Scribe_Collections.Look(ref jobTypeList, "jobTypeList", LookMode.Value, LookMode.Value, ref jobTypes, ref jobBoolValue);
+            Scribe_Values.Look(ref allPossibleJobs, "allPossibleJobs", false, true);
+
+        }
+        public void DoWindowContents(Rect inRect)
+        {
+            List<string> keys = jobTypeList.Keys.Reverse().ToList();
+            Listing_Standard ls = new Listing_Standard();
+            Rect rect = new Rect(inRect.x, inRect.y, inRect.width, inRect.height);
+            Rect rect2 = new Rect(0f, 0f, inRect.width - 30f, inRect.height - 30f);
+            Widgets.BeginScrollView(rect, ref scrollPos, rect2, true);
+            ls.ColumnWidth = rect2.width ;
+            ls.Begin(rect2);
+            ls.CheckboxLabeled("allowAllJobs".Translate().CapitalizeFirst(), ref allPossibleJobs, null);
+            for (int num = keys.Count - 1; num >= 0; num--)
+            {
+                bool test = jobTypeList[keys[num]];
+                if (DefDatabase<SkillDef>.GetNamedSilentFail(keys[num]) == null)
+                {
+                    jobTypeList.Remove(keys[num]);
+                }
+                else
+                {
+                    ls.CheckboxLabeled("SettingEnable".Translate(keys[num].ToString()), ref test);
+                    jobTypeList[keys[num]] = test;
+                }
+            }
+
+            ls.End();
+            Widgets.EndScrollView();
+            base.Write();
+        }
+    }
+}
